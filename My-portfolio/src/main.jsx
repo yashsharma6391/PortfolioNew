b1c76186d9aa1d7eaa6e625553ea699175e84be5
yashsharma6391 <ys7097569@gmail.com>
2025-11-19 23:59:05 +0530

    Update main.jsx

--- My-portfolio/src/main.jsx
+++ My-portfolio/src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./blob.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-    
-  </HashRouter>
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
 );
